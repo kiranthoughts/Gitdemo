a04d822658a7b5b4b6e1e3495e7569fd95b4998f
Kiran <kiran.sriv@gmail.com>
2020-05-23 02:45:29 +0100

    this is usa guy

--- src/test/java/MavenProject/selenium1Test.java
+++ src/test/java/MavenProject/selenium1Test.java
@@ -9,13 +9,18 @@ public class selenium1Test {
 	{
 		System.out.println("hi this is selenium element one");
 		System.out.println("hi this is selenium element oneone");
+		System.out.println("hi this is selenium element oneone1");
+		System.out.println("hi this is selenium element oneone11");
 		
+
 
 	}
 	@Test 
 	public void uiobject1()
 	{
 		System.out.println("hi this is selenium object one");
+		System.out.println("hi this is selenium object oneone");
+		System.out.println("hi this is selenium object oneoneone");
 		
 
 	}
