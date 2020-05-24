e069eb1be3620f2f8058ae202670f3242d005b63
Kiran <kiran.sriv@gmail.com>
2020-05-24 01:25:01 +0100

    this is archdevelop

---
+++ src/test/java/MavenProject/archselenium.java
@@ -0,0 +1,15 @@
+package MavenProject;
+
+import org.testng.annotations.Test;
+
+public class archselenium {
+	@Test 
+	public void archdevelop1()
+	{
+		System.out.println("hi this is selenium archdevelop1 one");
+		System.out.println("hi this is selenium archdevelop1 oneone");
+		System.out.println("hi this is selenium archdevelop1 oneone1");
+		System.out.println("hi this is selenium archdevelop1 oneone11");
+	}
+
+}
